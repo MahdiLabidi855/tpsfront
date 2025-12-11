17269ac2711bd941b5625181c94efe6d02337ee5
Mahdi LAABIDI <mahdi.laabidi@teamwillgroup.com>
2025-12-11 12:14:59 +0100

    WORKSHOP2

--- react-frontend-lab/frontend/src/components/Header.js
+++ react-frontend-lab/frontend/src/components/Header.js
@@ -4,8 +4,10 @@ const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <Link className="navbar-brand" to="/">Mon App</Link>
+      <Link className="navbar-brand" to="/">Mon App</Link>
         <div className="navbar-nav">
+      <Link className="nav-link" to="/users">Users</Link>
+      <Link className="nav-link" to="/form">Form</Link>
           <Link className="nav-link" to="/">Accueil</Link>
           <Link className="nav-link" to="/about">À propos</Link>
         </div>
